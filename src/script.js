2cff90f20d0aee53c323f9fba9698a74c8c21979
nightmare14226 <miltonjohnston14226>
2023-12-07 16:46:57 +0900

    fix: model name

--- src/script.js
+++ src/script.js
@@ -102,7 +102,7 @@ const gltfLoader = new GLTFLoader();
 gltfLoader.setDRACOLoader(dracoLoader);
 
 // Load main model
-gltfLoader.load("/models/model.glb", (gltf) => {
+gltfLoader.load("/models/city_v2.glb", (gltf) => {
     model = gltf.scene;
     scene.add(model);
 });
